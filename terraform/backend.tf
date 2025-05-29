c6a056756559afde0e1463365d2f5f11ce709e62
prudviA <165309155+prudviA@users.noreply.github.com>
2025-05-29 23:10:58 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-prudvi"
+    bucket         = "terraform-state-prudvi789654"
     key            = "eks/devsecops/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
